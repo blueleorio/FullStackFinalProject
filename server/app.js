e248e81900a9d35df0e488ab4764e739e5c5a81d
Thanh Hoang <vietthanh456@gmail.com>
2024-03-20 23:12:17 +0700

    GroundBreaking

--- server/app.js
+++ server/app.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api", indexRouter);
+const mongoose = require("mongoose");
+const mongoURI = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("first connection to the database");
+  })
+  .catch((err) => {
+    console.log("error connecting to the database: ", err);
+  });
 
 module.exports = app;
