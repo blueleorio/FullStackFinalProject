6a808394d90ebbfa00b84735336157e4beabfba8
Thanh Hoang <vietthanh456@gmail.com>
2024-03-21 01:55:25 +0700

    Pacing for 5k, gonna be a long way to be at 42k pace.

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, Tab, Tabs } from "@mui/material";
+import PhoneIcon from "@mui/icons-material/Phone";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PersonPinIcon from "@mui/icons-material/PersonPin";
 
 function HomePage() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
+      >
         <Typography variant="h3" component="h1" gutterBottom>
           Home Page
         </Typography>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="icon label tabs example"
+        >
+          <Tab icon={<PhoneIcon />} label="SLACKER" />
+          <Tab icon={<FavoriteIcon />} label="DOER" />
+          <Tab icon={<PersonPinIcon />} label="HARDER" />
+        </Tabs>
       </Grid>
     </Container>
   );
