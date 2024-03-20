68cb9d4575278d5fad778e9a67936175adb9b05a
Thanh Hoang <vietthanh456@gmail.com>
2024-03-21 00:03:43 +0700

    mock-up Design

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./routes";
-import { AuthProvider } from "./contexts/AuthContext";
+import Router from "./routes/index";
 import ThemeProvider from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <ThemeProvider>
-          <Router />
-        </ThemeProvider>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
